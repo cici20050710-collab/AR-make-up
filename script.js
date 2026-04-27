a4e037aaf1399cc8dd8cd8b053557ce1ca134405
cici20050710-collab <cici20050710@gmail.com>
2026-04-28 00:00:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function selectCategory(cat){
-    currentCategory = cat;
 
-    // 清掉全部樣式
-    document.getElementById("btn-base").classList.remove("text-pink-600");
-    document.getElementById("btn-eye").classList.remove("text-pink-600");
-    document.getElementById("btn-lip").classList.remove("text-pink-600");
-
-    // 高亮目前選擇
-    if(cat==="底妝") document.getElementById("btn-base").classList.add("text-pink-600");
-    if(cat==="眼妝") document.getElementById("btn-eye").classList.add("text-pink-600");
-    if(cat==="唇妝") document.getElementById("btn-lip").classList.add("text-pink-600");
-
-    console.log("目前選擇:", cat);
-}
